20cc5fbc38aa70d74c401dccf3e6d1b639a102dd
csooi <cs.ooi@espressoft.com.my>
2023-11-23 16:19:16 +0800

    Initial Commit

---
+++ TimelineSwiftUI/TimelineSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimelineSwiftUIApp.swift
+//  TimelineSwiftUI
+//
+//  Created by Chien Shing Ooi on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TimelineSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
