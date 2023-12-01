84ded9aeb000bcdfc92643c157f47e8559b0ff4c
Sean <zx.chow@espressoft.com.my>
2023-12-01 13:09:21 +0800

    SPIKE Remove ZStack from pill

--- TimelineSwiftUI/ContentView.swift
+++ TimelineSwiftUI/ContentView.swift
@@ -3,50 +3,46 @@ import SwiftUI
 struct TimelineView: View {
     @ObservedObject var viewModel: TimelineViewModel
     var body: some View {
-            LazyHStack(alignment: .top, spacing: 0) {
-                ForEach(0..<42) { week in
-                    VStack(alignment: .leading) {
-                        Text("Week \(week + 1)").frame(maxWidth: .infinity, alignment: .center)
-                        
-                        HStack(alignment: .center, spacing: 1) {
-                            ZStack() {
-                                Rectangle()
-                                    .fill(Color.white)
-                                    .frame(width: UIScreen.main.bounds.width)
-                                VStack(alignment: .leading) {
-                                    ForEach(0..<maxRow(in: viewModel.timePills), id: \.self) { row in
-                                        if let pill = viewModel.timePillForRowAndWeek(row: row, week: week) {
-                                            ZStack(alignment: .center) {
-                                                Rectangle()
-                                                    .fill(Color(pill.color ?? UIColor.red))
-                                                    .cornerRadius(10)
-                                                    .frame(width: CGFloat(pill.duration ?? 0)/7.0 *  weekWidth(), height: 30)
-                                                    .onTapGesture {
-                                                        print("Tapped")
-                                                    }
-                                                Text((pill.body ?? "ttiel") + " \(pill.duration ?? 0)" )
-                                                    .font(.system(size: 20.0, weight: .bold))
-                                                    .padding(.horizontal, 10.0)
-                                                    .foregroundColor(.white)
-                                                    .fixedSize(horizontal: false, vertical: false)
-                                                    .frame(width: CGFloat(pill.duration ?? 0)/7.0 * weekWidth())
-                                                    .padding(.vertical, 20.0)
-                                                    .lineLimit(3)
-                                            }
-                                        }
-                                    }
-                                    
-                                    
-                                }
-                            }
-                        }.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-                    }
-                }
+        LazyHStack(alignment: .top, spacing: 0) {
+            ForEach(0..<42) { week in
+                VStack(alignment: .leading) {
+                    Text("Week \(week + 1)").frame(maxWidth: .infinity, alignment: .center)
                     
-            }.modifier(ScrollingHStackModifier(
-                items: 42,
-                itemWidth: UIScreen.main.bounds.width,
-                itemSpacing: 0))
+                    HStack(alignment: .center, spacing: 1) {
+                        ZStack() {
+                            Rectangle()
+                                .fill(Color.white)
+                                .frame(width: UIScreen.main.bounds.width)
+                            VStack(alignment: .leading) {
+                                ForEach(0..<maxRow(in: viewModel.timePills), id: \.self) { row in
+                                    if let pill = viewModel.timePillForRowAndWeek(row: row, week: week) {
+                                        Text((pill.body ?? "ttiel") + " \(pill.duration ?? 0)" )
+                                            .font(.system(size: 20.0, weight: .bold))
+                                            .padding(.horizontal, 10.0)
+                                            .foregroundColor(.white)
+                                            .fixedSize(horizontal: false, vertical: false)
+                                            .frame(width: CGFloat(pill.duration ?? 0)/7.0 * weekWidth())
+                                            .padding(.vertical, 20.0)
+                                            .multilineTextAlignment(.leading)
+                                            .lineLimit(3)
+                                            .background(RoundedRectangle(cornerRadius: 10).fill(Color(pill.color ?? UIColor.red)))
+                                            .onTapGesture {
+                                                print("Tapped")
+                                            }
+                                    }
+                                }
+                                
+                                
+                            }
+                        }
+                    }.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                }
+            }
+            
+        }.modifier(ScrollingHStackModifier(
+            items: 42,
+            itemWidth: UIScreen.main.bounds.width,
+            itemSpacing: 0))
         
 //        GeometryReader { geometry in
 //            ScrollView(.horizontal, showsIndicators: false) {
