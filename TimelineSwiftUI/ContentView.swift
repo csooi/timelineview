1b7f120c202f66b076bf6a1f380d0896b144c502
csooi <cs.ooi@espressoft.com.my>
2023-12-08 00:00:39 +0800

    Corrected part of the UI based on Hi-Fi specs

--- TimelineSwiftUI/ContentView.swift
+++ TimelineSwiftUI/ContentView.swift
@@ -8,58 +8,62 @@ struct TimelineUIConstants {
 
 struct TimelineView: View {
     @ObservedObject var viewModel: TimelineViewModel
+    @State var currentIndex: CGFloat = 0.0
+    let bleen = UIColor(red: 0, green: 0.75, blue: 0.86, alpha: 1.0)
+    let bombayLB = Color(red: 0.69, green: 0.69, blue: 0.71)
+    let concreteLB = Color(red: 0.95, green: 0.95, blue: 0.97)
     var body: some View {
         GeometryReader { geometry in
             ZStack {
                 Rectangle()
-                    .frame(width:geometry.size.width , height: 2)
-                    .foregroundColor(Color.gray)
+                    .frame(width:geometry.size.width , height: 3)
+                    .foregroundColor(Color(red: 0.95, green: 0.95, blue: 0.97))
                     .position(CGPoint(x: geometry.size.width/2 ,
                                       y: TimelineUIConstants.positionOfWeeksZstackElements ))
                 Text("WEEKS")
-                    .font(.system(size: 20.0, weight: .bold))
-                    .foregroundColor(.blue)
+                    .font(.system(size: 12.0, weight: .bold))
+                    .foregroundColor(Color(bleen))
                     .background(Color.white)
                     .position(CGPoint(x: geometry.size.width/2 ,
                                       y: TimelineUIConstants.positionOfWeeksZstackElements))
                     .padding([.leading, .trailing], 4)
                 
-                Text("11 Dec - 18 Dec") //TBD: calculate and update
-                    .foregroundColor(.black)
-                    .padding([.leading, .trailing], 10)
-                    .padding([.top, .bottom], 4)
-                
-                    .background(
-                        Capsule()
-                            .fill(Color.gray)
-                    )
-                    .position(CGPoint(x: geometry.size.width/2 ,
-                                      y: TimelineUIConstants.positionOfWeeksZstackElements+30))
-                
             }
             LegacyScrollView(.horizontal, showsIndicators: false) {
                 ZStack {
-                    Rectangle()
-                        .fill(Color.red)
-                        .frame(width: 2, height: geometry.size.height)
-                        .position(x: weekWidth(geometry.size.width) * CGFloat(viewModel.currentWeek + 1) + weekWidth(geometry.size.width)/2.0,
-                                  y: geometry.size.height / 2)
-                        .offset(y: 210)
+                    VStack {
+                        Circle().fill(Color(red: 1, green: 0.3, blue: 0.39))
+                            .frame(width: 10)
+                            .position(x: weekWidth(geometry.size.width) * CGFloat(viewModel.currentWeek + 1) + weekWidth(geometry.size.width)/2.0,
+                                                                           y: geometry.size.height / 2)
+//                        .offset(y: 80)
+                        Rectangle()
+                            .fill(Color.red)
+                            .frame(width: 2, height: geometry.size.height)
+                            .position(x: weekWidth(geometry.size.width) * CGFloat(viewModel.currentWeek + 1) + weekWidth(geometry.size.width)/2.0,
+                                      y: geometry.size.height / 2)
+//                        .offset(y: 80)
+                    }.offset(y: -geometry.size.height / 2 + 80)
+                    
                     VStack(alignment: .leading) {
                         //Week header
                         
-                        Spacer().frame(height: 120)
+                        Spacer()
                         HStack(spacing: 0) {
                             ForEach(0..<43, id: \.self) { week in
                                 Text("\(week)")
+                                    .font(
+                                        Font.system(size: week == Int(currentIndex) ? 28 : 18)
+                                            .weight(.semibold)
+                                    )
                                     .frame(width: weekWidth(geometry.size.width))
-                                    .foregroundColor(.black)
+                                    .foregroundColor(week == Int(currentIndex) ? Color(bleen) : bombayLB)
                                     .onTapGesture {
                                         print("week tapped")
                                     }
                             }
                         }
-                        Spacer().frame(height: 80)
+                        Spacer().frame(height: 70)
                         ForEach(0..<maxRow(in: viewModel.timePills), id: \.self) { row in
                             HStack(alignment: .top, spacing: 0) {
                                 ForEach(0..<43, id: \.self) { week in
@@ -91,31 +95,58 @@ struct TimelineView: View {
             .background(
                 VStack {
                     Rectangle()
-                        .fill(Color.gray.opacity(0.2))
-                        .frame(width: 2, height: geometry.size.height)
-                        .position(x: geometry.size.width/3, y: geometry.size.height / 2)
-                        .offset(y: 80)
+                        .fill(LinearGradient(gradient: Gradient(colors: [Color.white, Color(red: 0.95, green: 0.95, blue: 0.97).opacity(1), Color.white]),
+                                             startPoint: .top,
+                                             endPoint: .bottom))
+                        .frame(width: 2,
+                               height: geometry.size.height)
+                        .position(x: geometry.size.width/3,
+                                  y: geometry.size.height/2)
+                        .offset(y: 0)
                     Rectangle()
-                        .fill(Color.gray.opacity(0.2))
+                        .fill(LinearGradient(gradient: Gradient(colors: [Color.white, Color(red: 0.95, green: 0.95, blue: 0.97).opacity(1), Color.white]),
+                                             startPoint: .top,
+                                             endPoint: .bottom))
                         .frame(width: 2, height: geometry.size.height)
                         .position(x: geometry.size.width/3 * 2, y: 0)
-                        .offset(y: 80)
+                        .offset(y: 0)
                 }
             )
+            Text("11 Dec - 18 Dec") //TBD: calculate and update
+                .font(Font.custom("SF Pro", size: 10))
+                .multilineTextAlignment(.center)
+                .foregroundColor(.black)
+                .padding([.leading, .trailing], 10)
+                .padding([.top, .bottom], 4)
+            
+                .background(
+                    Capsule()
+                        .fill(concreteLB)
+                        .frame(width: 100)
+                )
+                .position(CGPoint(x: geometry.size.width/2 ,
+                                  y: TimelineUIConstants.positionOfWeeksZstackElements+30))
         }
     }
     
     func snapWith(scrollView: UIScrollView) {
         let segmentWidth = UIScreen.main.bounds.size.width / 3
         let offset = scrollView.contentOffset.x
-        let index = round(offset / segmentWidth)
-        let newOffset = index * segmentWidth
+        currentIndex = round(offset / segmentWidth)
+        let newOffset = currentIndex * segmentWidth
         UIView.animate(withDuration: 0.3, animations: {
             scrollView.contentOffset = CGPoint(x: newOffset, y: 0)
         })
     }
 
-    
+    func scrollToIndex(_ index: Int, scrollView: UIScrollView) {
+        let segmentWidth = UIScreen.main.bounds.size.width / 3
+        let newOffsetX = CGFloat(index) * segmentWidth
+        UIView.animate(withDuration: 0.3, animations: {
+            scrollView.contentOffset = CGPoint(x: newOffsetX, y: 0)
+        })
+    }
+
     func weekWidth(_ screenWidth: CGFloat) -> CGFloat {
         // Calculate the width for each week column
         screenWidth / 3
@@ -134,7 +165,8 @@ struct PillView: View {
     var body: some View {
         Rectangle()
             .fill(Color(pill.color ?? UIColor.red))
-            .cornerRadius(10)
+            .cornerRadius(8)
+            .padding(.horizontal, 4.0)
             .frame(width: CGFloat(Int(pill.duration ?? 0)/7) * widthPerWeek)
             .onTapGesture {
                 print("Tapped")
@@ -145,13 +177,13 @@ struct PillView: View {
                         pillGeometries[pill.id] = newFrame
                     }
             })
-        Text((pill.body ?? "ttiel"))
-            .font(.system(size: 20.0, weight: .bold))
+        Text((pill.body ?? ""))
+            .font(.system(size: 16.0, weight: .medium))
             .padding(.horizontal, 10.0)
             .foregroundColor(.white)
             .fixedSize(horizontal: false, vertical: true)
             .frame(width: (CGFloat(Int(pill.duration ?? 0)/7).rounded() * widthPerWeek))
-            .padding(.vertical, 20.0)
+            .padding(.vertical, 10.0)
             .lineLimit(3)
     }
     
